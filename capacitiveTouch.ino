17a5123b1a8bd6b018060f85afe15d5d76a06a4b
agan <AAGAN@users.noreply.github.com>
2015-01-01 23:36:53 -0500

    You can select buckets here

--- capacitiveTouch.ino
+++ capacitiveTouch.ino
@@ -24,6 +24,13 @@
 #define TFT_DC 9
 #define TFT_CS 10
 
+int nx=10;
+int ny=10;
+int pxx=(240/nx)*3;
+int pyy=(320-240)+(240/ny)*3;
+int xx = 0;
+int yy = 0;
+
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // The FT6206 uses hardware I2C (SCL/SDA)
@@ -35,14 +42,14 @@ void setup() {
 
   tft.begin();
   tft.fillScreen(ILI9341_BLACK);
- // Serial.begin(9600);
+  Serial.begin(9600);
 
   if (! ctp.begin(40)) {  // pass in 'sensitivity' coefficient
 //    Serial.println("Couldn't start FT6206 touchscreen controller");
     while (1);
   }
   tft.setRotation(2);
-  drawGrid(10 ,10); 
+  drawGrid(nx ,ny); 
 }
 
 
@@ -52,10 +59,35 @@ void loop(void) {
   }
   // Retrieve a point  
   TS_Point p = ctp.getPoint();
+  int width= 240/nx;
+  int height= 240/ny;
+  if ( xx != (p.x/width)*width || yy != ((p.y-(320-240))/height)*height+(320-240)){
+    if (p.y > (320-240-1) && (p.y != pyy || p.x != pxx)){
+      highlight(p.x,p.y,nx,ny,ILI9341_RED);
+      highlight(pxx,pyy,nx,ny,ILI9341_BLACK);
+      pxx = p.x;
+      pyy = p.y;
+      xx = (p.x/width)*width;
+      yy = ((p.y-(320-240))/height)*height+(320-240);
+    }
+  }
+  
 //  tft.fillScreen(ILI9341_BLACK);
-  tft.fillRect(85,0,155,8,ILI9341_BLACK);
+  tft.fillRect(0,0,240,8,ILI9341_BLACK);
   testText(p.x, p.y);  
-  tft.drawPixel(p.x,p.y,ILI9341_YELLOW);  
+//  tft.drawPixel(p.x,p.y,ILI9341_YELLOW);  
+}
+
+void highlight(int x, int y, int nx, int ny, uint16_t color){
+  
+  int xx,yy,width,height;
+  width=240/nx;
+  height=240/ny;
+  xx = (x/width)*width;
+  yy = ((y-(320-240))/height)*height+(320-240);
+  tft.fillRect(xx,yy,width,height,color);
+  tft.drawRect(xx,yy,width,height,ILI9341_YELLOW);
+
 }
 
 void drawGrid(int i, int j){
@@ -73,16 +105,14 @@ void drawGrid(int i, int j){
 
 void testText(int x, int y) {
 
-  tft.setCursor(85, 0);
+  tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_WHITE);  
   tft.setTextSize(1);
 
-
   tft.print("x= ");
   tft.print(x);
-  tft.print("  ");
 
-  tft.print("y= ");
+  tft.print(" y= ");
   tft.println(y);
 }   
 
